b59d3b11c12bfa70205286c8781dd1a9377d6187
Braanah Craig <braanah@presence.io>
2017-12-19 15:13:12 -0500

    fixed date/time logic

--- Scripts/Components/dashboard.component.ts
+++ Scripts/Components/dashboard.component.ts
@@ -1,17 +1,15 @@
 module PDigitalSignage {
     export class DashboardComponent {
-        rawEventData: any = [];
-        eventList: any = [];
-        thisWeekRandomized: any = [];
-        todaysEventsRandomized: any = [];
-        thisWeeksEvents: any = [];
-        todaysEvents: any = [];
-        counter: number = 0;
+        rawEventData: any = []
+        eventList: any = []
+        topFourThisWeek: any = []
+        topFiveToday: any = []
+        thisWeeksEvents: any = []
+        todaysEvents: any = []
         domainName = '';
         parent: any;
         designDetails: any = {};
         iqrurl: string;
-        isStarted: boolean = false;
 
         static $inject = [
             '$http',
@@ -61,76 +59,13 @@ module PDigitalSignage {
         }  
 
         getEventList() { 
-            var won = (response) => {
-                this.rawEventData = [];
-                this.eventList = [];
-
-                this.thisWeekRandomized = [];
-                this.todaysEventsRandomized = [];
-                this.thisWeeksEvents = [];
-                this.todaysEvents = [];
-                this.rawEventData = response.data;
-                var today = new Date();
-                for(var event of this.rawEventData){
-                    var b = moment.utc(today);
-                    var a = moment.utc(event.startDateTimeUtc);
-                    var c = moment.utc(event.endDateTimeUtc);
-                    var days = a.diff(b, 'days');
-                    var hours = a.diff(b, 'hours');
-                    console.log(hours);
-                    if((this.eventList.indexOf(event)) == -1){
-                        if(days > -1){
-                            this.eventList.push(event);
-                        }
-                    }
-                    this.eventList.sort(function(a, b) {
-                        const firstEvent = a.startDateTimeUtc;
-                        const secondEvent = b.startDateTimeUtc;
-                      
-                        let comparison = 0;
-                        if (firstEvent > secondEvent) {
-                          comparison = 1;
-                        } else if (firstEvent < secondEvent) {
-                          comparison = -1;
-                        }
-                        return comparison;
-                    });
-                }
-                for(var event of this.eventList){
-                    var b = moment.utc(today);
-                    var a = moment.utc(event.startDateTimeUtc);
-                    var c = moment.utc(event.endDateTimeUtc);
-                    var days = a.diff(b, 'days');
-                    var hours = a.diff(b, 'hours');
-                        if(days > -1 && days < 1 && hours < 5){
-                            this.todaysEvents.push(event);
-                        }
-                        if(days > 1 && days <  7){
-                            this.thisWeeksEvents.push(event);
-                        }
-                }
-                
-                var eventCount = this.thisWeeksEvents.length;
-                for(var i = 0; i < 4 && i < this.thisWeeksEvents.length; i++){
-                        this.thisWeekRandomized[i] = this.thisWeeksEvents[i];
-                }
-                if(this.todaysEvents.length > 0){
-                    for(var i = 0; i < this.todaysEvents.length && i < 5; i++){
-                            this.todaysEventsRandomized.push(this.todaysEvents[i]);
-                    }
-                }
-                console.log(this.todaysEventsRandomized, "random");
-                console.log(this.thisWeekRandomized);
-                console.log(this.thisWeeksEvents);
-                console.log(this.eventList);
-            }
-            var lost = (response) => {
-                if(response == null){
-                    console.log("Nothing was retrieved from API")
-                }
-            }
-            return this.dashboardData.getEvents(this.domainName)
-                .then(won)
+            this.rawEventData = this.dashboardData.rawEventData;
+            this.eventList = this.dashboardData.eventList;
+            this.topFourThisWeek = this.dashboardData.topFourThisWeek;
+            this.topFiveToday = this.dashboardData.topFiveToday;
+            this.thisWeeksEvents = this.dashboardData.thisWeeksEvents;
+            this.todaysEvents = this.dashboardData.todaysEvents;
+            return this.dashboardData.getEvents(this.domainName);
         }
         
     }
